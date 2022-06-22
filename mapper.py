0fe0ccb1a85eeeba940bd9e86384baf809db9e35
choodnovsky <choodnovsky1978@gmail.com>
2022-06-22 12:56:43 +0300

    добавил лемматизацию

--- mapper.py
+++ mapper.py
@@ -1,8 +1,12 @@
 import sys
+from pymystem3 import Mystem
+m = Mystem() # создаем объект лемматизатора
+
 inputfile = sys.stdin  # принимаем данные из считанного сырого файла в консоли
 next(inputfile)  # пропускаем заголовок
 for line in inputfile:
     phrase = line.split(',')[0]  # получаем текст запроса без числа упоминаний
-    for word in phrase.strip().split(' '):  # еще раз делим фразу на отдельные слова
+    phrase = m.lemmatize(phrase.strip())[:-1] # приводим слова запроса к 1 словоформе и отбрасываем перенос строки
+    for word in phrase:  # еще раз делим фразу на отдельные слова
         if word.isalpha():  # если слово состоит из букв
             print(f'{word}')  # выводим в печать
--- reducer.py
+++ reducer.py
@@ -1,13 +1,10 @@
 import sys
-from pymystem3 import Mystem
 
-m = Mystem()
 counter = {}  # заводим пустой словарь
 inputfile = sys.stdin
 for line in inputfile:  # принимаем данные из консоли
     line = line.strip()  # отбрасываем пробельные символы с краев
-    line = m.lemmatize(line)[0]  # приводим слово к простой форме, чтобы исключить влияние падежей и склонений
-    if line != '' and line in counter:  # если такой ключ уже есть в словаре, прибавляем 1
+    if line in counter:  # если такой ключ уже есть в словаре, прибавляем 1
         counter[line] += 1
     else:  # иначе создаем новый ключ
         counter[line] = 1
