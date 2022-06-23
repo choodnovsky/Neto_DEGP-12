68e6d14094a8331c9af3aa07edb6d330435287af
choodnovsky <choodnovsky1978@gmail.com>
2022-06-23 07:38:04 +0300

    поменял таки со словарей на сравнение с предыдущим

--- reducer.py
+++ reducer.py
@@ -1,12 +1,18 @@
 import sys
 
-counter = {}  # заводим пустой словарь
+prev = None
+cnt = 0
 inputfile = sys.stdin  # принимаем данные из консоли
 for line in inputfile:
     line = line.strip()  # отбрасываем пробельные символы с краев
-    if line in counter:  # если такой ключ в словаре уже есть, прибавляем 1
-        counter[line] += 1
-    else:  # иначе создаем новый ключ
-        counter[line] = 1
-for key, val in counter.items():
-    print(f'слово {key} встретилось {val} раз')  # выводим подсчеты слов на печать
+    if prev:  # если слово не пустое
+        if prev == line:  # так же проверяем равно ли оно текущему
+            cnt += 1  # добавляем к счетчику 1
+        else:
+            print(f'слово {prev} встретилось {cnt} раз') # если не расно выводим накопленое на печать
+            prev = line # обновляем слово и счетчик
+            cnt = 1
+    else:
+        prev = line  # если пустое, присваиваем первое слово
+        cnt = 1
+print(f'слово {prev} встретилось {cnt} раз')  # выводим подсчеты слов на печать
